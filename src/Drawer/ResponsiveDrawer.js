16881cb2ab38b5fcf9500620f8a1a62e54b8a736
Doug L <douglaswleonard+git@gmail.com>
2018-11-16 15:13:30 -0700

    remove unused var

--- src/Drawer/ResponsiveDrawer.js
+++ src/Drawer/ResponsiveDrawer.js
@@ -4,7 +4,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
